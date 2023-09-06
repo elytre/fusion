8b0084b89317f97020aa5b9b045de021cfd6fce8
Clément Latzarus <c@ltzr.io>
2023-09-06 20:26:13 +0200

    build: bootstrap next.js app

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Fusion',
+  description: 'Le site de la collection Fusion, par les éditions L\'Atalante',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
