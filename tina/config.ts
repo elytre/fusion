83725de9668e5d9d8333b1f01dbc149a28b54255
Clément Latzarus <c@ltzr.io>
2023-09-18 21:25:52 +0200

    feat: add and configure tina cms

---
+++ tina/config.ts
@@ -0,0 +1,55 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "861a030e-1046-4ad6-9e4e-bd99501fbf88", // Get this from tina.io
+  token: "237676788db2aa2b12133ebc1a451b3c62846e77", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "news",
+        label: "Actualités",
+        path: "_site/news",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Titre",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date",
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+        ui: {
+          // This is an DEMO router. You can remove this to fit your site
+          router: ({ document }) => `/news/${document._sys.filename}`,
+        },
+      },
+],
+  },
+});
