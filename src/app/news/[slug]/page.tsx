46d44560780eaf234d83c00d388cb1cf28530694
Clément Latzarus <c@ltzr.io>
2023-09-19 08:59:39 +0200

    refactor: extract methods to load posts

--- src/app/news/[slug]/page.tsx
+++ src/app/news/[slug]/page.tsx
@@ -1,8 +1,5 @@
-import {join} from "path";
-import fs from "fs";
-import {NewsPost as NewsPostType} from "@/types";
-import matter from "gray-matter";
 import {NewsPost} from "@/components/NewsPost";
+import {getAllNewsPostSlugs, getNewsPostBySlug} from "@/lib/api";
 
 type RouteParams = {
   params: {
@@ -10,20 +7,17 @@ type RouteParams = {
   };
 }
 
-const newsDirectory = join(process.cwd(), '_site/news');
-const newsPostsFilenames = fs.readdirSync(newsDirectory);
-
 // noinspection JSUnusedGlobalSymbols
 export async function generateStaticParams() {
-  return newsPostsFilenames.map((filename: string) => {
-    return { slug: filename.replace('.md', '') };
+  return getAllNewsPostSlugs().map((slug) => {
+    return { slug };
   });
 }
 
 // noinspection JSUnusedGlobalSymbols
 export async function generateMetadata({ params }: RouteParams) {
   const { slug: requestSlug } = params;
-  const newsPost = _loadNewsPost(requestSlug);
+  const newsPost = getNewsPostBySlug(requestSlug);
 
   if (!newsPost) {
     return {};
@@ -36,21 +30,9 @@ export async function generateMetadata({ params }: RouteParams) {
 
 export default function NewsPostPage({ params }: RouteParams) {
   const { slug: requestSlug } = params;
-  const newsPost = _loadNewsPost(requestSlug);
+  const newsPost = getNewsPostBySlug(requestSlug);
   return <div className="news-post-page">
     <NewsPost post={newsPost} />
   </div>;
 }
 
-function _loadNewsPost(slug: string): NewsPostType {
-  const newsFilePath = `${newsDirectory}/${slug}.md`;
-  const fileContents = fs.readFileSync(newsFilePath, 'utf8');
-  const { data, content} = matter(fileContents);
-  return {
-    title: data.title,
-    date: data.date,
-    image: data.image,
-    html: Boolean(data.html),
-    content,
-  };
-}
