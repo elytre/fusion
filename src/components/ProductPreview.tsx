58d61d14bb0c57698d83c079c76bedf701c7ba89
Clément Latzarus <c@ltzr.io>
2023-09-24 16:53:35 +0200

    feat: add link on cover in ProductPreview

--- src/components/ProductPreview.tsx
+++ src/components/ProductPreview.tsx
@@ -3,8 +3,6 @@ import Trans from './Trans';
 import Image from "next/image";
 import Link from 'next/link';
 
-import Price from "@/components/Price";
-
 import {
   Product as ProductType,
 } from '@/types';
@@ -16,25 +14,25 @@ export type ProductProps = {
 export default function ProductPreview({
                                   product,
                                 }: ProductProps): React.ReactElement {
-  const locale = "fr";
-
   return (
     <div className="ProductPreview">
       {product.coverImage ? (
         <div className="ProductPreview-cover">
-          <Image
-            src={product.coverImage}
-            alt={product.title}
-            width={1024}
-            height={1448}
-            className="ProductPreview-cover-image"
-          />
+          <Link href={`/fr/p/${product.slug}`}>
+            <Image
+              src={product.coverImage}
+              alt={product.title}
+              width={1024}
+              height={1448}
+              className="ProductPreview-cover-image"
+            />
+          </Link>
         </div>
       ) : null}
 
       <div className="ProductPreview-infos">
         <h1 className="ProductPreview-title">
-          <Link href={`/${locale}/p/${product.slug}`}>{product.title}</Link>
+          <Link href={`/fr/p/${product.slug}`}>{product.title}</Link>
         </h1>
 
         <p className="ProductPreview-author">
@@ -46,15 +44,4 @@ export default function ProductPreview({
       </div>
     </div>
   );
-}
-
-function _buildBuyLink(buyLink: string) {
-  return <a
-    className='ProductPreview-buy-link-button'
-    href={buyLink}
-    target='_blank'
-    rel='noopener noreferrer'
-  >
-    <Trans>Buy</Trans>
-  </a>;
 }
