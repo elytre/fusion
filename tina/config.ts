2abee6dc7e70ce783e4d0069ff88f8d7c2b735b3
Clément Latzarus <c@ltzr.io>
2023-09-24 08:38:59 +0200

    feat(cms): add Products collection

--- tina/config.ts
+++ tina/config.ts
@@ -60,6 +60,134 @@ export default defineConfig({
           },
         ],
       },
-],
+      {
+        name: "products",
+        label: "Livres",
+        path: "_site/catalog",
+        fields: [
+          {
+            type: "string",
+            name: "ean",
+            label: "ISBN",
+          },
+          {
+            type: "string",
+            name: "title",
+            label: "Titre",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "string",
+            name: "author",
+            label: "Auteur·trice",
+            required: true,
+          },
+          {
+            type: "datetime",
+            name: "releaseDate",
+            label: "Date de publication",
+            required: true,
+          },
+          {
+            type: "number",
+            name: "price",
+            label: "Prix",
+          },
+          {
+            type: "number",
+            name: "pageCount",
+            label: "Nombre de pages",
+          },
+          {
+            type: "string",
+            name: "originalLanguage",
+            label: "Langue d'origine",
+            options: [
+              {label: "Allemand", value: "de"},
+              {label: "Anglais", value: "en"},
+              {label: "Français", value: "fr"},
+            ]
+          },
+          {
+            label: "Contributeur·trice·s",
+            name: "contributors",
+            type: "object",
+            list: true,
+            fields: [
+              {
+                label: "Nom",
+                name: "name",
+                type: "string"
+              },
+              {
+                label: "Role",
+                name: "role",
+                type: "string",
+                options: [
+                  {label: "Auteur·trice", value: "Author"},
+                  {label: "Traducteur·trice", value: "Translator"},
+                  {label: "Illustrateur·trice de couverture", value: "Cover artist"},
+                  {label: "Photographe", value: "Photographer"},
+                ]
+              },
+            ]
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Quatrième de couverture",
+            isBody: true,
+          },
+          {
+            label: "Avis",
+            name: "reviews",
+            type: "object",
+            list: true,
+            fields: [
+              {
+                label: "Citation",
+                name: "text",
+                type: "string"
+              },
+              {
+                label: "Auteur·trice",
+                name: "author",
+                type: "string"
+              },
+              {
+                label: "Source",
+                name: "source",
+                type: "string",
+              },
+            ]
+          },
+          {
+            label: "Extras",
+            name: "extras",
+            type: "object",
+            list: true,
+            fields: [
+              {
+                label: "Type",
+                name: "type",
+                type: "string",
+                options: [{ label: "Youtube", value: "youtube" }],
+              },
+              {
+                label: "Titre",
+                name: "title",
+                type: "string"
+              },
+              {
+                label: "Lien",
+                name: "href",
+                type: "string",
+              },
+            ]
+          },
+        ],
+      },
+    ],
   },
 });
