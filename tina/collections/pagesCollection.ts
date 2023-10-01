bcc3315928d890e1059117d4e5e49d27c2dc9ad5
Clément Latzarus <c@ltzr.io>
2023-10-01 20:44:04 +0200

    feat: add title field to page

--- tina/collections/pagesCollection.ts
+++ tina/collections/pagesCollection.ts
@@ -3,12 +3,27 @@ import { Collection } from 'tinacms';
 import { TextBlock } from '../blocks/TextBlock';
 import { BannerBlock } from '../blocks/BannerBlock';
 import { ProductCarouselBlock } from '../blocks/ProductCarouselBlock';
+import slugify from "slugify";
 
 export const pagesCollection: Collection = {
-  name: "pages",
-  label: "Pages",
-  path: "_site/pages",
+  name: 'pages',
+  label: 'Pages',
+  path: '_site/pages',
+  ui: {
+    filename: {
+      slugify: (values) => {
+        return values.title ? slugify(values.title, {lower: true}) : "";
+      },
+    },
+  },
   fields: [
+    {
+      type: 'string',
+      name: 'title',
+      label: 'Titre',
+      isTitle: true,
+      required: true,
+    },
     {
       type: 'object',
       list: true,
