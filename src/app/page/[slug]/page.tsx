fda5ce8a60794dbd2fbcc6ff4916aa4c6d6bbc79
Clément Latzarus <c@ltzr.io>
2023-10-02 07:04:03 +0200

    feat: add custom page controller

---
+++ src/app/page/[slug]/page.tsx
@@ -0,0 +1,45 @@
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
+
+import { RouteParams } from "@/types";
+import { getCustomPageBySlug, pageSlugs } from "@/lib/api/pages";
+import { CustomPage } from "@/app/components/CustomPage";
+
+// noinspection JSUnusedGlobalSymbols
+export async function generateStaticParams() {
+  return pageSlugs.map((slug) => {
+    return { slug };
+  });
+}
+
+// noinspection JSUnusedGlobalSymbols
+export async function generateMetadata({ params }: RouteParams) {
+  const { slug } = params;
+  if (!pageSlugs.includes(slug)) {
+    return {};
+  }
+
+  const page = getCustomPageBySlug(slug);
+  return {
+    title: `${page.title} - Fusion`,
+  }
+}
+
+export default function CustomPagePage({ params }: RouteParams) {
+  const { slug } = params;
+  if (!pageSlugs.includes(slug)) {
+    return notFound();
+  }
+
+  const page = getCustomPageBySlug(slug);
+
+  // noinspection HtmlUnknownTarget
+  return <div className="news-post-page">
+    <nav className="breadcrumbs">
+      <Link href="/">Accueil</Link> {'>'} <span className="current">{page.title}</span>
+    </nav>
+
+    <CustomPage page={page} />
+  </div>;
+}
+
