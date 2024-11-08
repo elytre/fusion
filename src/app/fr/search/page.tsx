c3674313af7d71b6e8e49c16a3cbfd2e2bee3c0e
Clément Latzarus <c@ltzr.io>
2024-11-08 22:01:38 +0100

    fix: handle breaking change in NextJS 14

--- src/app/fr/search/page.tsx
+++ src/app/fr/search/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 
 import { getSearchIndex } from '@/lib/user-files';
@@ -9,7 +9,7 @@ import { Product } from "@/types";
 
 const searchIndex = getSearchIndex();
 
-export default function SearchResultsView(): React.ReactElement {
+function SearchResults(): React.ReactElement {
   const searchParams = useSearchParams();
   const searchQuery = searchParams?.get('q') ?? '';
   const products = _getProductsMatchingQuery(searchQuery);
@@ -21,9 +21,15 @@ export default function SearchResultsView(): React.ReactElement {
   const productsSortedByMostRecent = products
     .sort((product1, product2) => (product1.releaseDate > product2.releaseDate ? -1 : 1))
 
+  return <ProductList products={productsSortedByMostRecent}/>;
+}
+
+export default function SearchResultsView(): React.ReactElement {
   return (
     <div className="SearchResultsView">
-      <ProductList products={productsSortedByMostRecent} />
+      <Suspense>
+        <SearchResults />
+      </Suspense>
     </div>
   );
 }
