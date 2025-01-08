72693d39e7dee0ba9d625ae9ec266205ab0c377c
Clément Latzarus <c@ltzr.io>
2025-01-08 09:30:26 +0100

    feat: use daily frequency for home and news page

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,4 +4,15 @@ module.exports = {
   generateRobotsTxt: true,
   generateIndexSitemap: false,
   outDir: 'out',
+  changefreq: 'monthly',
+  transform: async (config, path) => {
+    const isDailyPage = path === '/' || path === '/fr/page/actualites'
+    return {
+      loc: path,
+      changefreq: isDailyPage ? 'daily' : config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    }
+  },
 }
