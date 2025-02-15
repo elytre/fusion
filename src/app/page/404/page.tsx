c7e6e278b8647dd00789b0d55ee69ab874d5a93e
Clément Latzarus <c@ltzr.io>
2025-02-15 09:22:03 +0100

    feat: add 404 error page

---
+++ src/app/page/404/page.tsx
@@ -0,0 +1,11 @@
+export default function Error404Page() {
+  return (
+    <div>
+      <h1>Erreur 404</h1>
+      <p>La page n’existe pas.</p>
+      <p>
+        <a href="/">Retour à l’accueil</a>
+      </p>
+    </div>
+  );
+}
